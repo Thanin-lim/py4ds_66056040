eeb4e3d38c138fc58ce5755f63512f45971102f3
comseven <66056040@kmitl.ac.th>
2023-10-08 11:24:33 +0700

    All Complete Test

--- src/ex04.py
+++ src/ex04.py
@@ -3,63 +3,30 @@ Execise 4
 """
 
 
-def area(param1, param2):
-    """
-    Calculates the area of a rectangle given its length and width.
+def area(L, W):
+    result=L*W
+    return result
 
-    Parameters:
-        length (float): The length of the rectangle.
-        width (float): The width of the rectangle.
-
-    Returns:
-        float: The area of the rectangle.
-    """
-    # TODO : complete this
-    pass
+    # Fix : complete this
 
 
-def perimetero(param1, param2):
-    """
-    Calculate the perimeter of a rectangle.
 
-    Args:
-        length (float): The length of the rectangle.
-        width (float): The width of the rectangle.
-
-    Returns:
-        float: The perimeter of the rectangle.
-    """
-    # TODO : complete this
-    pass
+def perimeter(L,W):
+    result=(L+W)+(L+W)
+    return result
+    # Fix : complete this
 
 
-def volume(param1, param2, param3):
-    """
-    Calculates the volume of an object given its length, width, and height.
 
-    Args:
-        length (float): The length of the object.
-        width (float): The width of the object.
-        height (float): The height of the object.
-
-    Returns:
-        float: The volume of the object.
-    """
-    # TODO : complete this
-    pass
+def volume(L,W,H):
+   result=L*W*H
+   return result
+    # Fix : complete this
 
 
-def surface_area(param1, param2, param3):
-    """
-    Calculate the surface area of a rectangular prism.
 
-    Parameters:
-        length (float): The length of the rectangular prism.
-        width (float): The width of the rectangular prism.
-        height (float): The height of the rectangular prism.
+def surface_area(L,W,H):
+    result=2*(L*W)+2*(W*H)+2*(L*H)
+    return result
+    # Fix : complete this
 
-    Returns:
-        float: The total surface area of the rectangular prism.
-    """
-    # TODO : complete this
-    pass
