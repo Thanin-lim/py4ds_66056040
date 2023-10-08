d522f52fb35f0277cc49c7beb9648f30ca17d7e3
comseven <66056040@kmitl.ac.th>
2023-10-08 10:27:00 +0700

    All Complete Test

--- src/ex01.py
+++ src/ex01.py
@@ -5,7 +5,7 @@ Execise 1
 #input
 def greeting():
    print("สวัสดีชาวลาดกระบัง")
-    # TODO : complete this
+    # Fix : complete this
 
 
 
@@ -14,14 +14,14 @@ def know_my_name():
     print(n)
     return n
 
-    # TODO : complete this
+    # Fix : complete this
     pass
 
 
 def say_hi(name=None):
     name='เจษ'
     print('สวัสดีคุณ'+name)
-    # TODO : complete this
+    # Fix : complete this
     pass
 
 
