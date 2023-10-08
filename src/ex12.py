ed057edc542a2be4cfe43dca844134e1622d4cf4
comseven <66056040@kmitl.ac.th>
2023-10-08 09:43:41 +0700

    ADD Test & EX

---
+++ src/ex12.py
@@ -0,0 +1,19 @@
+"""
+Execise 12
+"""
+
+
+def get_smallest(num_list):
+    num_lists=min(num_list)
+    return num_lists
+    # """
+    # Get the smallest number from a list of numbers.
+    # Args:
+    #     num_list (list): A list of numbers.
+    #
+    # Returns:
+    #     int or None: The smallest number from the list. If the list is empty, returns None.
+    # """
+    pass
+
+#%%
