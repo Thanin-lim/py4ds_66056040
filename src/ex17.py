f13fef0e3ff5bcea41e66b586b63f8bb45fca0ba
comseven <66056040@kmitl.ac.th>
2023-10-11 12:15:11 +0700

    All Complete Test

--- src/ex17.py
+++ src/ex17.py
@@ -4,14 +4,11 @@ Exercise 17
 import random
 
 
-def roll_dice(params):
-    """
-    Calculate the total sum of rolling a certain number of dice.
-
-    Parameters:
-        num_of_dice (int): The number of dice to roll.
-
-    Returns:
-        int: The total sum of the dice rolls.
-    """
+def roll_dice(n):
+    f=0
+    for i in range(n):
+        f+=random.randint(1,6)
+    return f
     pass
+
+#%%
