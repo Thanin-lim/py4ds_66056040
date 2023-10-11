5a9920413a149cd09735e91eb889f2eb498305b0
comseven <66056040@kmitl.ac.th>
2023-10-11 11:19:31 +0700

    All Complete Test

--- src/ex07.py
+++ src/ex07.py
@@ -3,17 +3,10 @@ Execise 7
 """
 
 
-def print_ASCII_table():
-    """
-    Generate and print the ASCII table for a range of characters.
+def print_ASCII_table(start_char,end_char):
+    for i in range(start_char,end_char+1):
+        print(chr(i))
 
-    Args:
-        start_char (int): The ASCII value of the starting character.
-        end_char (int): The ASCII value of the ending character.
 
-    Returns:
-        str: Returns nothing. The function simply prints the ASCII characters.
+    # Fix : complete this
 
-    """
-    # TODO : complete this
-    pass
