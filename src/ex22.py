1e10734e70d1f305b72dc1586532604e481989c6
comseven <66056040@kmitl.ac.th>
2023-10-11 11:49:48 +0700

    All Complete Test

--- src/ex22.py
+++ src/ex22.py
@@ -3,16 +3,21 @@ Exercise 22 : Rock Paper Scissor
 """
 
 
-def rps_check(param, param1):
-    """
-    Check the result of a rock-paper-scissors game between two players.
+def rps_check(name, name1):
+    if name =='rock' and name1=='paper':
+        return 'player two'
+    elif name =='rock' and name1=='scissors':
+        return 'player one'
+    elif name =='paper' and name1=='scissors':
+        return 'player two'
+    elif name =='paper' and name1=='rock':
+        return  'player one'
+    elif name =='scissors' and name1=='rock':
+        return 'player two'
+    elif name =='scissors' and name1=='paper':
+        return 'player one'
+    else:
+        return 'tie'
 
-    Parameters:
-        p1 (str): The choice of player one. It can be 'rock', 'paper', or 'scissor'.
-        p2 (str): The choice of player two. It can be 'rock', 'paper', or 'scissor'.
-
-    Returns:
-        str: The result of the game. It can be 'player one', 'player two', or 'tie'.
-    """
-    # TODO : complete this
+    # Fix : complete this
     pass
