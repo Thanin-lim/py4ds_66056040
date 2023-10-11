4c61316a5beb2e2464961e32b4a48ee4d2ce8f72
comseven <66056040@kmitl.ac.th>
2023-10-11 12:10:50 +0700

    All Complete Test

--- src/ex24.py
+++ src/ex24.py
@@ -7,14 +7,13 @@ meridiem : midday
 
 
 def get_time_every_15_min():
-    """
-    Generate a time string every 15 minutes.
+    meridims=['am','pm']
+    hours=['12','1','2','3','4','5','6','7','8','9','10','11']
+    minit=['00','15','30','45']
+    for a in meridims:
+        for h in hours:
+            for m in minit:
+                print(h+":"+m+" "+a)
 
-    This function iterates through the meridiems, hours, and minutes to generate a time string
-    for every 15 minutes. It prints the time string in the format "hour:minute meridiem".
-
-    Returns:
-        str: The generated time string.
-    """
     # TODO : complete this
     pass
