26a55f0e11cf749c8c28247332fea8d7dfe5e75c
comseven <66056040@kmitl.ac.th>
2023-10-12 08:02:42 +0700

    All Complete Test

--- src/ex25.py
+++ src/ex25.py
@@ -15,17 +15,16 @@ Exercise 25 : Multiplication Table
 """
 
 
-def multiplication_table(param, param1 = 11):
-    """
-    Generates a multiplication table from the given start to the end number.
+def multiplication_table(start, end = 11):
 
-    Args:
-        start (int): The starting number of the multiplication table.
-        end (int, optional): The ending number of the multiplication table. Defaults to 11.
 
-    Returns:
-        None: This function does not return anything.
-    """
     print('  | 1  2  3  4  5  6  7  8  9 10')
     print('--+------------------------------')
-    # TODO : complete this
+    # fix : complete this
+    count = 10
+    for i in range(start, end):
+        result = ' '
+        for j in range(count):
+            result += '{:2d}'.format((j + 1) * i)+' '
+        result = '{:2d}'.format(i)+'|'+result
+        print(result)
