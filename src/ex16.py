082cc6bd4c305a5702a1f10bf479b6631e2826db
comseven <66056040@kmitl.ac.th>
2023-10-12 09:19:31 +0700

    All Complete Test

--- src/ex16.py
+++ src/ex16.py
@@ -3,14 +3,22 @@ Exercise 16
 """
 
 
-def mode(params):
-    """
-    Calculate the mode of a list of numbers.
 
-    Parameters:
-    - num_list (list): A list of numbers.
+def mode(num):
+    n_len = len(num)
+    if n_len == 0:
+        return None
+    max_count = 0
+    element_having_max_freq = 0
+    for i in range(0, n_len):
+        count = 0
+        for j in range(0, n_len):
+            if num[i] == num[j]:
+                count += 1
+        if count > max_count:
+            max_count = count
+            element_having_max_freq = num[i]
 
-    Returns:
-    - int or None: The mode of the list, or None if the list is empty.
-    """
-    pass
+    return element_having_max_freq
+
+#%%
--- src/ex18.py
+++ src/ex18.py
@@ -3,5 +3,14 @@ Exercise 18 : Buy 8 get 1 free
 """
 
 
-def get_cost_of_coffee(param, param1):
+def get_cost_of_coffee(unit, price):
+    free = unit // 9
+    paid = unit - free
+    return paid * price
+    pass
+
+def get_cost_of_coffee_2(unit, price):
+    free = unit // 9
+    paid = unit - free
+    return paid * price
     pass
