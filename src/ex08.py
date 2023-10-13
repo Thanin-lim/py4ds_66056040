d657a48a754380170774b5dca48f729dc3edbe92
comseven <66056040@kmitl.ac.th>
2023-10-13 11:53:33 +0700

    All Complete Test

--- src/ex08.py
+++ src/ex08.py
@@ -3,45 +3,24 @@ Execise 8
 """
 
 
-def write_to_file(param, param1):
-    """
-    Write the given message to a file with the provided filename.
-
-    Args:
-        filename (str): The name of the file to write to.
-        message (str): The message to write to the file.
-
-    Returns:
-        None
-    """
-    # TODO : complete this
+def write_to_file(filename, message):
+    f = open(filename, 'w')
+    f.write(message)
+    # fix : complete this
     pass
 
 
-def read_from_file(param):
-    """
-    Read the contents of a file.
+def read_from_file(filename):
 
-    Args:
-        filename (str): The name of the file to read.
-
-    Returns:
-        str: The contents of the file.
-    """
-    # TODO : complete this
+    f = open(filename, 'r')
+    return f.read()
+    # fix : complete this
     pass
 
 
-def append_to_file(param, param1):
-    """
-    Append the given message to the end of the specified file.
-
-    Args:
-        filename (str): The name of the file to append the message to.
-        message (str): The message to append to the file.
-
-    Returns:
-        None: This function does not return anything.
-    """
-    # TODO : complete this
+def append_to_file(filename, message):
+    f = open(filename, 'a')
+    f.write(message)
+    # fix : complete this
     pass
+
